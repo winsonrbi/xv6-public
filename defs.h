10d85f42701bb6ab1125b71ab6caf15faf048d7e
Winson <wbi002@ucr.edu>
2020-10-25 12:53:22 -0700

    Fixed project 1

--- defs.h
+++ defs.h
@@ -186,7 +186,4 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 
-//test.c
-//int 			info(int);
-// number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
--- test.c
+++ test.c
@@ -6,8 +6,20 @@ int
 main(void)
 {
 	printf(1,"Testing lab 1 syscall info number of processes running %d\n",info(1));
-	printf(1,"Testing lab 1 syscall info total number of syscalls %d\n",info(2));
-	printf(1,"Testing lab 1 syscall info total number of memory page %d\n",info(3));
-	printf(1,"Printing pid %d \n", getpid());
+	int pid = fork();
+	if (pid == 0)
+	{
+		printf(1,"Testing after fork, number of processes runing now is %d\n",info(1));
+	}
+	else{
+		wait();
+		printf(1,"Testing after child process is terminated, number of processes running now is %d\n",info(1));
+		printf(1,"Testing lab 1 syscall info total number of syscalls %d\n",info(2));
+		//account for count from zero
+		counter = counter + 1;
+		printf(1,"Testing after allocating int array of size %d, total number of memory pages %d\n",counter,info(3));
+		printf(1,"Printing pid %d \n", getpid());
+		
+	}
 	exit();
 }
